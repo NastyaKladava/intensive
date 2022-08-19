64f7d4748f60b197eaafa04bc9b2ffee5c3e8e14
Nastya Kladova <nstsologub@gmail.com>
2022-08-19 15:44:10 +0200

    add some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,9 @@ import Form from "./components/Form/Form";
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-header-title">Создание анкеты</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-header-title'>Создание анкеты</h1>
         </header>
         <Form />
       </div>
